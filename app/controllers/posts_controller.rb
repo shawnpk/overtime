37b3c5f83c1f9d7bc3903d498e1ba3bfe0f72cca
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-08-08 18:22:24 -0700

    Implemented user association to posts with tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
+    @post.user_id = current_user.id
 
     if @post.save
       redirect_to @post, notice: 'Your post has been created successfully'
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -17,6 +17,8 @@ describe 'navigate to post' do
 
   describe 'creation' do
     before do
+      user = User.create email: 'test@test.com', password: 'password', password_confirmation: 'password', first_name: 'Shawn', last_name: 'Kearney'
+      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
@@ -31,5 +33,14 @@ describe 'navigate to post' do
 
       expect(page).to have_content('Some rationale')
     end
+
+     it 'will have a user associated with it' do
+       fill_in 'post[date]', with: Date.today
+       fill_in 'post[rationale]', with: 'User Association'
+
+       click_on 'Save'
+
+       expect(User.last.posts.last.rationale).to eq('User Association')
+     end
   end
 end
