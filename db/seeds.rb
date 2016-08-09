f0de286f91a631bd17153f11a938171e0fb654a4
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-08-08 19:09:49 -0700

    Added posts to index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create email: 'test@test.com', password: 'password', password_confirmation: 'password', first_name: 'Shawn', last_name: 'Kearney'
+
+puts '1 user created'
+
 100.times do |post|
-  Post.create! date: Date.today, rationale: "#{post} rationale content"
+  Post.create! date: Date.today, rationale: "#{post} rationale content", user_id: @user.id
 end
 
 puts '100 posts have been created'
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,24 +1,35 @@
 require 'rails_helper'
 
 describe 'navigate to post' do
-  describe ' index' do
-    it 'can be reached successfully' do
-      visit posts_path
+  before do
+    user = User.create email: 'test@test.com', password: 'password', password_confirmation: 'password', first_name: 'Shawn', last_name: 'Kearney'
+    login_as(user, :scope => :user)
+  end
 
+  describe ' index' do
+    before do
+      visit posts_path
+    end
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
+      expect(page).to have_content 'Posts'
+    end
+
+    it 'has a list of posts' do
+      post1 = Post.create date:Date.today, rationale: 'Post1'
+      post2 = Post.create date:Date.today, rationale: 'Post2'
+
       visit posts_path
 
-      expect(page).to have_content 'Posts'
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
     before do
-      user = User.create email: 'test@test.com', password: 'password', password_confirmation: 'password', first_name: 'Shawn', last_name: 'Kearney'
-      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
