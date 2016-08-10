5d3bb53536a4216ea989d50091f161376ba6b1c3
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-08-10 16:10:16 -0700

    Implemented edit functionality for status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 @user = User.create email: 'test@test.com', password: 'password', password_confirmation: 'password', first_name: 'Shawn', last_name: 'Kearney'
 
-@admin_user = User.create email: 'admin@test.com', password: 'password', password_confirmation: 'password', first_name: 'Shawn', last_name: 'Kearney', type: 'AdminUser'
-
 puts '1 user created'
+
+AdminUser.create email: 'admin@test.com', password: 'password', password_confirmation: 'password', first_name: 'Admin', last_name: 'User'
+
+puts '1 admin_user created'
 
 100.times do |post|
   Post.create! date: Date.today, rationale: "#{post} rationale content", user_id: @user.id
