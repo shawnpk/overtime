12bbf6b26e037d0c0f122b13b807a8d7b93c730c
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-08-12 17:39:38 -0700

    Implemented labels with status values on index for posts

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,6 +1,6 @@
 class PostPolicy < ApplicationPolicy
   def update?
-    return true if  post_approved? && admin?
+    return true if post_approved? && admin?
     return true if user_or_admin && !post_approved?
   end
 
