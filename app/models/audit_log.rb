f0a88b44f33d0ef6398300fe19ed029378d618eb
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-08-14 14:46:29 -0700

    Integrated validations and defaults for audit log attributes

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,14 @@
 class AuditLog < ActiveRecord::Base
   belongs_to :user
+
+  validates :user_id,     presence: true
+  validates :status,      presence: true
+  validates :start_date,  presence: true
+
+  after_initialize :set_defaults
+
+  private
+    def set_defaults
+      self.start_date ||= (Date.today - 6.days)
+    end
 end
