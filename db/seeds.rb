39f54f11a8790d549cea00e16360d1d52a6efd4e
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-08-16 09:38:49 -0700

    Added rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
-@user = User.create email: 'test@test.com',
+@employee = Employee.create email: 'test@test.com',
                     password: 'password',
                     password_confirmation: 'password',
-                    first_name: 'Shawn',
-                    last_name: 'Kearney',
+                    first_name: 'David',
+                    last_name: 'Webb',
                     phone: '3108641460'
 
-puts '1 user created'
+puts '1 employee created'
 
 AdminUser.create email: 'admin@test.com',
                  password: 'password',
                  password_confirmation: 'password',
                  first_name: 'Admin',
-                 last_name: 'User',
+                 last_name: 'Name',
                  phone: '3108641460'
 
-puts '1 admin_user created'
+puts '1 Admin user created'
 
-AuditLog.create! user_id: @user.id, status: 0, start_date: (Date.today - 6.days)
-AuditLog.create! user_id: @user.id, status: 0, start_date: (Date.today - 13.days)
-AuditLog.create! user_id: @user.id, status: 0, start_date: (Date.today - 20.days)
+AuditLog.create! user_id: @employee.id, status: 0, start_date: (Date.today - 6.days)
+AuditLog.create! user_id: @employee.id, status: 0, start_date: (Date.today - 13.days)
+AuditLog.create! user_id: @employee.id, status: 0, start_date: (Date.today - 20.days)
 
 puts '3 audit logs have been created'
 
 100.times do |post|
-  Post.create! date: Date.today, rationale: "#{post} rationale content: Duis anim est aute excepteur aliqua culpa pariatur occaecat pariatur occaecat. Quis dolore id reprehenderit aute ipsum esse ut sunt non eiusmod veniam. Sit aute eu voluptate enim ut irure occaecat incididunt id ea voluptate eu consequat eiusmod ex dolore ex.", user_id: @user.id, overtime_request: 2.5
+  Post.create! date: Date.today, rationale: "#{post} rationale content: Duis anim est aute excepteur aliqua culpa pariatur occaecat pariatur occaecat. Quis dolore id reprehenderit aute ipsum esse ut sunt non eiusmod veniam. Sit aute eu voluptate enim ut irure occaecat incididunt id ea voluptate eu consequat eiusmod ex dolore ex.", user_id: @employee.id, overtime_request: 2.5
 end
 
 puts '100 posts have been created'
