56adf74abee73f52e08ff644d5ccdada7473fd13
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-03-06 17:04:55 -0800

    Udate seeds file and changed number of audit log items shown on admin index page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
   def home
     if admin_types.include?(current_user.type)
       @pending_approvals = Post.submitted
-      @recent_audit_items = AuditLog.last(10)
+      @recent_audit_items = AuditLog.all
     else
       @pending_audit_confirmations = current_user.audit_logs.pending.by_start_date
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ AdminUser.create email: 'admin@test.com',
                  password: 'password',
                  password_confirmation: 'password',
                  first_name: 'Admin',
-                 last_name: 'Name',
+                 last_name: 'User',
                  phone: '2134567890'
 
 puts '1 Admin user created'
