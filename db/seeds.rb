33b7f00d950c5263b17c81c70a7f940d51327cc3
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-03-06 16:55:48 -0800

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,39 @@
 @employee = Employee.create email: 'test@test.com',
                     password: 'password',
                     password_confirmation: 'password',
-                    first_name: 'David',
-                    last_name: 'Webb',
-                    phone: '3108641460'
+                    first_name: 'Tony',
+                    last_name: 'Stark',
+                    phone: '2028889999'
 
-puts '1 employee created'
+@employee2 = Employee.create email: 'test2@test.com',
+                    password: 'password',
+                    password_confirmation: 'password',
+                    first_name: 'Bruce',
+                    last_name: 'Wayne',
+                    phone: '2028889999'
+
+@employee3 = Employee.create email: 'test3@test.com',
+                    password: 'password',
+                    password_confirmation: 'password',
+                    first_name: 'Clark',
+                    last_name: 'Kent',
+                    phone: '6095557777'
+
+@employee4 = Employee.create email: 'test4@test.com',
+                    password: 'password',
+                    password_confirmation: 'password',
+                    first_name: 'Peter',
+                    last_name: 'Parker',
+                    phone: '2123334444'
+
+@employee5 = Employee.create email: 'test5@test.com',
+                    password: 'password',
+                    password_confirmation: 'password',
+                    first_name: 'Bruce',
+                    last_name: 'Banner',
+                    phone: '2130003333'
+
+puts '5 employees created'
 
 AdminUser.create email: 'admin@test.com',
                  password: 'password',
@@ -20,10 +48,42 @@ AuditLog.create! user_id: @employee.id, status: 0, start_date: (Date.today - 6.d
 AuditLog.create! user_id: @employee.id, status: 0, start_date: (Date.today - 13.days)
 AuditLog.create! user_id: @employee.id, status: 0, start_date: (Date.today - 20.days)
 
-puts '3 audit logs have been created'
+AuditLog.create! user_id: @employee2.id, status: 0, start_date: (Date.today - 6.days)
+AuditLog.create! user_id: @employee2.id, status: 0, start_date: (Date.today - 13.days)
+AuditLog.create! user_id: @employee2.id, status: 0, start_date: (Date.today - 20.days)
 
-100.times do |post|
+AuditLog.create! user_id: @employee3.id, status: 0, start_date: (Date.today - 6.days)
+AuditLog.create! user_id: @employee3.id, status: 0, start_date: (Date.today - 13.days)
+AuditLog.create! user_id: @employee3.id, status: 0, start_date: (Date.today - 20.days)
+
+AuditLog.create! user_id: @employee4.id, status: 0, start_date: (Date.today - 6.days)
+AuditLog.create! user_id: @employee4.id, status: 0, start_date: (Date.today - 13.days)
+AuditLog.create! user_id: @employee4.id, status: 0, start_date: (Date.today - 20.days)
+
+AuditLog.create! user_id: @employee5.id, status: 0, start_date: (Date.today - 6.days)
+AuditLog.create! user_id: @employee5.id, status: 0, start_date: (Date.today - 13.days)
+AuditLog.create! user_id: @employee5.id, status: 0, start_date: (Date.today - 20.days)
+
+puts '15 audit logs have been created'
+
+20.times do |post|
   Post.create! date: Date.today, rationale: "#{post} rationale content: Duis anim est aute excepteur aliqua culpa pariatur occaecat pariatur occaecat. Quis dolore id reprehenderit aute ipsum esse ut sunt non eiusmod veniam. Sit aute eu voluptate enim ut irure occaecat incididunt id ea voluptate eu consequat eiusmod ex dolore ex.", user_id: @employee.id, overtime_request: 2.5
+end
+
+20.times do |post|
+  Post.create! date: Date.today, rationale: "#{post} rationale content: Duis anim est aute excepteur aliqua culpa pariatur occaecat pariatur occaecat. Quis dolore id reprehenderit aute ipsum esse ut sunt non eiusmod veniam. Sit aute eu voluptate enim ut irure occaecat incididunt id ea voluptate eu consequat eiusmod ex dolore ex.", user_id: @employee2.id, overtime_request: 2.5
+end
+
+20.times do |post|
+  Post.create! date: Date.today, rationale: "#{post} rationale content: Duis anim est aute excepteur aliqua culpa pariatur occaecat pariatur occaecat. Quis dolore id reprehenderit aute ipsum esse ut sunt non eiusmod veniam. Sit aute eu voluptate enim ut irure occaecat incididunt id ea voluptate eu consequat eiusmod ex dolore ex.", user_id: @employee3.id, overtime_request: 2.5
+end
+
+20.times do |post|
+  Post.create! date: Date.today, rationale: "#{post} rationale content: Duis anim est aute excepteur aliqua culpa pariatur occaecat pariatur occaecat. Quis dolore id reprehenderit aute ipsum esse ut sunt non eiusmod veniam. Sit aute eu voluptate enim ut irure occaecat incididunt id ea voluptate eu consequat eiusmod ex dolore ex.", user_id: @employee4.id, overtime_request: 2.5
+end
+
+20.times do |post|
+  Post.create! date: Date.today, rationale: "#{post} rationale content: Duis anim est aute excepteur aliqua culpa pariatur occaecat pariatur occaecat. Quis dolore id reprehenderit aute ipsum esse ut sunt non eiusmod veniam. Sit aute eu voluptate enim ut irure occaecat incididunt id ea voluptate eu consequat eiusmod ex dolore ex.", user_id: @employee5.id, overtime_request: 2.5
 end
 
 puts '100 posts have been created'
